6184ae8cdc5702c03aaaf40b0da8594075df09c3
marchlcy@gmail.com <marchlcy@gmail.com>
2026-02-08 01:40:50 +0900

    refactor: reorganize package structure

--- src/main/java/com/sparkLab/study/constant/FeedbackCommentType.java
+++
@@ -1,6 +0,0 @@
-package com.sparkLab.study.constant;
-
-public enum FeedbackCommentType {
-    MENTEE_QUESTION,
-    MENTOR_REPLY
-}
--- src/main/java/com/sparkLab/study/security/jwt/JwksController.java
+++ src/main/java/com/sparkLab/study/security/jwt/JwksController.java
@@ -4,7 +4,6 @@ import com.nimbusds.jose.jwk.JWKSet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
