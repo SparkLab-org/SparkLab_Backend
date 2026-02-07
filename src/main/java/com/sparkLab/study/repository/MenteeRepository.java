bf7c0b3dc248ef9d561771c3c24fe4472d0123c6
Yang-Boyun <boyunyang@naver.com>
2026-02-07 13:52:56 +0900

    Feat : 로그인 auth 추가

--- src/main/java/com/sparkLab/study/repository/MenteeRepository.java
+++ src/main/java/com/sparkLab/study/repository/MenteeRepository.java
@@ -4,5 +4,8 @@ import com.sparkLab.study.entity.Mentee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MenteeRepository extends JpaRepository<Mentee, Long> {
+    Optional<Mentee> findByAccount_AccountId(Long accountId);
 }
