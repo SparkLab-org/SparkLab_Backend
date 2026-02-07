28b994e8202e62fa171b18f5a7142792b3d2cefe
marchlcy@gmail.com <marchlcy@gmail.com>
2026-02-07 16:28:47 +0900

    fix : resolvev ersion mismatch open-api seagger, springboot

--- src/main/java/com/sparkLab/study/repository/MenteeRepository.java
+++ src/main/java/com/sparkLab/study/repository/MenteeRepository.java
@@ -4,7 +4,6 @@ import com.sparkLab.study.entity.Mentee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MenteeRepository extends JpaRepository<Mentee, Long> {
