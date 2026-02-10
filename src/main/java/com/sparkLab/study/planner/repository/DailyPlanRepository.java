ecf752c0e4952d280b2daa3a6b088cb7eafe13a9
Yang-Boyun <boyunyang@naver.com>
2026-02-10 15:50:39 +0900

    fix : 리스트로 반환

--- src/main/java/com/sparkLab/study/planner/repository/DailyPlanRepository.java
+++ src/main/java/com/sparkLab/study/planner/repository/DailyPlanRepository.java
@@ -4,14 +4,15 @@ import com.sparkLab.study.planner.entity.DailyPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
 import java.time.LocalDate;
-import java.util.Optional;
+import java.util.List;
 
 public interface DailyPlanRepository extends JpaRepository<DailyPlan, Long> {
 
     @Query("SELECT d.dailyPlanId FROM DailyPlan d WHERE d.mentee.menteeId = :menteeId AND d.planDate = :planDate")
-    Optional<Long> findIdByMenteeIdAndPlanDate(@Param("menteeId") Long menteeId,
-                                               @Param("planDate") LocalDate planDate);
+    List<Long> findIdByMenteeIdAndPlanDate(@Param("menteeId") Long menteeId,
+                                          @Param("planDate") LocalDate planDate);
 
-    Optional<DailyPlan> findByMentee_MenteeIdAndPlanDate(Long menteeMenteeId, LocalDate planDate);
+    List<DailyPlan> findByMentee_MenteeIdAndPlanDate(Long menteeMenteeId, LocalDate planDate);
 }
--- src/main/java/com/sparkLab/study/task/repository/FeedbackBookmarkRepository.java
+++ src/main/java/com/sparkLab/study/task/repository/FeedbackBookmarkRepository.java
@@ -4,11 +4,10 @@ import com.sparkLab.study.task.entity.FeedbackBookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface FeedbackBookmarkRepository extends JpaRepository<FeedbackBookmark, Long> {
     List<FeedbackBookmark> findByMentee_MenteeIdOrderByCreateTimeDesc(Long menteeId);
-    Optional<FeedbackBookmark> findByMentee_MenteeIdAndFeedback_FeedbackId(Long menteeId, Long feedbackId);
+    List<FeedbackBookmark> findByMentee_MenteeIdAndFeedback_FeedbackId(Long menteeId, Long feedbackId);
     boolean existsByMentee_MenteeIdAndFeedback_FeedbackId(Long menteeId, Long feedbackId);
     void deleteByMentee_MenteeIdAndFeedback_FeedbackId(Long menteeId, Long feedbackId);
 }
