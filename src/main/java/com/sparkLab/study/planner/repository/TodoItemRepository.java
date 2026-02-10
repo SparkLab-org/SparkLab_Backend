fe8282d69245699ca5b877c83789b9500a883331
marchlcy@gmail.com <marchlcy@gmail.com>
2026-02-10 19:31:27 +0900

    refactor: TodoItem 조회 성능 개선 (fetch 전략 수정)

--- src/main/java/com/sparkLab/study/planner/repository/TodoItemRepository.java
+++ src/main/java/com/sparkLab/study/planner/repository/TodoItemRepository.java
@@ -2,7 +2,10 @@ package com.sparkLab.study.planner.repository;
 
 import com.sparkLab.study.planner.entity.TodoItem;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,7 +16,24 @@ public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
 
     List<TodoItem> findByDailyPlan_PlanDateOrderByCreateTimeAsc(LocalDate planDate);
 
-    List<TodoItem> findByDailyPlan_Mentee_MenteeIdAndDailyPlan_PlanDateOrderByCreateTimeAsc(Long menteeId, LocalDate planDate);
+    //List<TodoItem> findByDailyPlan_Mentee_MenteeIdAndDailyPlan_PlanDateOrderByCreateTimeAsc(Long menteeId, LocalDate planDate);
 
     boolean existsByTodoItemIdAndMentee_MenteeId(Long todoItemId, Long menteeId);
+
+        @Query("""
+    select ti
+    from TodoItem ti
+    join fetch ti.mentee me
+    join fetch me.account
+    left join fetch ti.mentor mo
+    left join fetch mo.account
+    where me.menteeId = :menteeId
+    and ti.dailyPlan.planDate = :planDate
+    order by ti.createTime asc
+    """)
+        List<TodoItem> findTodoItemsWithRelations(
+                @Param("menteeId") Long menteeId,
+                @Param("planDate") LocalDate planDate
+        );
+
 }
